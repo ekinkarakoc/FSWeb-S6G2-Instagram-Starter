b64d1f41d5ae34bc59787c905d503b7f4e7f8ebd
ekin <ekinkarakoc1@gmail.com>
2023-11-04 02:49:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,48 @@
 */
 
 // State hook u import edin
-import React from "react";
+import React, { useState } from "react";
+import AramaCubugu from "./bilesenler/AramaCubugu/AramaCubugu";
+import Gonderiler from "./bilesenler//Gonderiler/Gonderiler";
+import sahteVeri from "./sahte-veri";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
 import "./App.css";
 
 const App = () => {
+  const [posts, setPosts] = useState(sahteVeri);
+  const [search, setSearch] = useState("");
+  const [myLikes, setMyLikes] = useState([]);
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
+  const searchChangeHandler = (event) => {
+    const { value } = event.target;
+    setSearch(value);
+    // todo arama fonksiyonu yazılacak ..
+    const searchResult = sahteVeri.filter((item) => {
+      return item.username.includes(value);
+    });
+    setPosts(searchResult);
+  };
 
   const gonderiyiBegen = (gonderiID) => {
+    const guncelPosts = posts.map((item) => {
+      if (item.id == gonderiID) {
+        if (!myLikes.includes(gonderiID)) {
+          item.likes++;
+          setMyLikes([...myLikes, gonderiID]);
+        } else {
+          item.likes--;
+          myLikes.splice(myLikes.indexOf(gonderiID), 1);
+          setMyLikes([...myLikes]);
+        }
+      }
+      return item;
+    });
+
+    setPosts(guncelPosts);
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
@@ -35,6 +65,8 @@ const App = () => {
       App Çalışıyor
       {/* Yukarıdaki metni projeye başladığınızda silin*/}
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
+      <AramaCubugu search={search} changeHandler={searchChangeHandler} />
+      <Gonderiler gonderiyiBegen={gonderiyiBegen} gonderiler={posts} />
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
   );
